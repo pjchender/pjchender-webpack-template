9c07fdeb22fc55e57d8e2b40e793246a56b7d96d
pjchender <pjchender@gmail.com>
2018-09-27 11:22:04 +0800

    Update webpack.config.js, also upgrade to Node10.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import './styles/application.scss';
 import 'bootstrap';
 import $ from 'jquery';
+import '@babel/polyfill';
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+
+  fetchData();
+  async function fetchData() {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos/1'
+    );
+    const data = await response.json();
+    console.log('response', data);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,16 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devMode = process.env.NODE_ENV !== "production";
+const devMode = process.env.NODE_ENV !== 'production';
 const paths = {
-  src: path.join(__dirname, "src"),
-  dist: path.join(__dirname, "dist")
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist')
 };
 
 module.exports = {
-  mode: 'development',
+  mode: devMode ? 'development' : 'production',
   devServer: {
     contentBase: './dist'
   },
@@ -20,50 +21,74 @@ module.exports = {
     app: './src/index.js'
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js'          // [name] 會被 entry 中的 key 換調
+    // [name] 會被 entry 中的 key 換調
+    filename: devMode ? 'js/[name].bundle.js' : 'js/[name].[hash].bundle.js',
+    path: path.dist,
+    publicPath: '/'
   },
   module: {
     rules: [
+      // 處理 SASS 檔案
       {
-        // process .scss files
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          // 如果希望開發環境就打包出 CSS 檔案，
+          // 可以直接使用 MiniCssExtractPlugin.loader，但可能會沒有 HMR
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          // 'postcss-loader',
+          'sass-loader'
+        ]
       },
+      // 使用 Babel
       {
-        // use babel
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'entry',
+                  targets: '> 0.25%, not dead'
+                }
+              ]
+            ] // End of presets
+          } // End of options
         }
-      }
+      } // End of Babel
     ]
   },
   optimization: {
+    // 在這裡使用 SplitChunksPlugin
     splitChunks: {
       cacheGroups: {
-        commons: {
+        // 把所有 node_modules 內的程式碼打包成一支 vendors.bundle.js
+        vendors: {
           test: /[\\/]node_modules[\\/]/i,
-          name: "vendors",
-          chunks: "all"
+          name: 'vendors',
+          chunks: 'all'
         }
       }
+    },
+    // 把 webpack runtime 也打包成一支 runtime.bundle.js
+    runtimeChunk: {
+      name: 'runtime'
     }
   },
   resolve: {
     extensions: ['.js']
   },
   plugins: [
-    new webpack.NamedModulesPlugin(),
+    new webpack.HashedModuleIdsPlugin(), // 避免所有的檔案 hash 都改變
     new webpack.HotModuleReplacementPlugin(),
     new CleanWebpackPlugin(paths.dist), // 清除 dist 的內容
-    new HtmlWebpackPlugin({             // 幫我們把 dist 中的 js 檔注入 html 當中
-      template: path.join(paths.src, "index.html"),
-      filename: path.join(paths.dist, "index.html"),
+    new HtmlWebpackPlugin({
+      // 幫我們把 dist 中的 js 檔注入 html 當中
+      template: path.join(paths.src, 'index.html'),
+      filename: path.join(paths.dist, 'index.html'),
       inject: true,
       hash: false,
       minify: {
@@ -73,12 +98,24 @@ module.exports = {
         minifyCSS: devMode ? false : true
       }
     }),
-    new webpack.optimize.SplitChunksPlugin(),
+    // 把所有的 SCSS 打包成一支單檔
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css'
+    }),
     new CopyWebpackPlugin([
       {
         from: './src/vendor',
         to: './vendor'
       }
     ])
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 8080,
+    index: 'index.html',
+    hot: true,
+    host: '0.0.0.0',   // 預設是 localhost，設定則可讓外網存取
+    // open: true      // 打開瀏覽器
+  }
 };
